9ad047ae980dc5449fe0c7e900a2f0e0af6dbc8d
Sheyla <Sheylapopovich@gmail.com>
2021-12-19 16:11:54 -0700

    created a playgame function with a window prompt for the opponent

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ console.log("Hey hey you linked me!");
 let opponentChoices = ["Rock", "Paper", "Scissors"];
 
 let myChoices = ["Rock", "Paper", "Scissors"];
+
+let playGame = function() {
+    //this window prompt will have opponent enter their selection
+    var opponentChoices = window.prompt("Enter R, P, or S:");
+}
