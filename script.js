34a52f831b7baf38a3d3196e09224a03ad6718c9
Sheyla <Sheylapopovich@gmail.com>
2021-12-19 16:16:27 -0700

    created a return so if the user selects cancel game ends

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ let myChoices = ["Rock", "Paper", "Scissors"];
 let playGame = function() {
     //this window prompt will have opponent enter their selection
     var opponentChoices = window.prompt("Enter R, P, or S:");
+
+    //this will end the game if cancel is pressed
+    if (!opponentChoices) {
+       return; 
+    }
 }
