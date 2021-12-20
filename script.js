83b5337466dbf3bf98479751fd9c20754747783a
Sheyla <Sheylapopovich@gmail.com>
2021-12-19 18:13:47 -0700

    changed all vars to let for consistancy

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-console.log("Hey hey you linked me!");
 let wins = 0;
 let ties = 0;
 let losses = 0;
 
+//an array consisting of options for user to pick from
 let options = ["R", "P", "S"];
 
-let playGame = function () {
+let playGame = function() {
   //this window prompt will have opponent enter their selection
-  var opponentChoice = window.prompt("Enter R, P, or S:");
+  let opponentChoice = window.prompt("Enter R, P, or S:");
 
   //this will end the game if cancel is pressed
   if (!opponentChoice) {
@@ -40,13 +40,17 @@ let playGame = function () {
     losses++;
     window.alert("You lost!");
   }
-
+  //this will show games stats
   window.alert(
     "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
   );
 
   //this will ask the user if they want to play again
   let playAgain = window.confirm("Play again?");
+
+  if (playAgain) {
+    playGame();
+  }
 };
 
 //this will initiate the game
