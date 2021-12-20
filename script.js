5b9af269ef4d879af01e380ed8d621f4b2d429cf
Sheyla <Sheylapopovich@gmail.com>
2021-12-19 17:16:21 -0700

    inserted line with method Mathrandom to get random index from array

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 console.log("Hey hey you linked me!");
 let wins = 0;
 let ties = 0;
-let losses= 0;
-
-
+let losses = 0;
 
 let options = ["R", "P", "S"];
 
-let playGame = function() {
-    //this window prompt will have opponent enter their selection
-    var opponentChoice = window.prompt("Enter R, P, or S:");
+let playGame = function () {
+  //this window prompt will have opponent enter their selection
+  var opponentChoice = window.prompt("Enter R, P, or S:");
 
-    //this will end the game if cancel is pressed
-    if (!opponentChoice) {
-       return; 
-    }
+  //this will end the game if cancel is pressed
+  if (!opponentChoice) {
+    return;
+  }
 
-    //used .toUpperCase method to compare inputs easier 
-    userChoice = userChoice.toUpperCase();
+  //used .toUpperCase method to compare inputs easier
+  userChoice = userChoice.toUpperCase();
 
+  var index = Math.floor(Math.random() * options.length);
+  var computerChoice = options[index];
 
-}
+  window.alert("The computer chose " + computerChoice);
+};
 
-//this will initiate the game 
-playGame()
+//this will initiate the game
+playGame();
