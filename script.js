24286a45efc2ed7f26946c793b777b4bc893935b
Sheyla <Sheylapopovich@gmail.com>
2021-12-19 18:24:49 -0700

    fixed userChoice not defined bug

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let options = ["R", "P", "S"];
 
 let playGame = function () {
   //this window prompt will have opponent enter their selection
-  let opponentChoice = window.prompt("Enter R, P, or S:");
+  let userChoice = window.prompt("Enter R, P, or S:");
 
   //this will end the game if cancel is pressed
   if (!opponentChoice) {
@@ -19,18 +19,18 @@ let playGame = function () {
 
   //Math.random will allow for a random index to be selected from array
   let index = Math.floor(Math.random() * options.length);
-  let opponentChoice = options[index];
+  let computerChoice = options[index];
 
-  window.alert("The computer chose " + opponentChoice);
+  window.alert("The computer chose " + computerChoice);
 
   //this if statement is if user and opponent selects same choice
-  if (userChoice === opponentChoice) {
+  if (userChoice === computerChoice) {
     ties++;
     window.alert("It's a tie!");
   } else if (
-    (userChoice === "R" && opponentChoice === "S") ||
-    (userChoice === "P" && opponentChoice === "R") ||
-    (userChoice === "S" && opponentChoice === "p")
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "p")
   ) {
     wins++;
     window.alert("You win!");
